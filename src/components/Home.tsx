7db1f67108dd4bec4f28b9230423c05e83a7f404
Karthik Vishal <karthikvishal1506@gmail.com>
2025-07-20 18:23:18 +0530

    Added Router function for activity tab as well

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,6 +4,13 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div>
+        <div>
+            <h1>Hello</h1>
+        </div>
+
+
+
+
       <Link to='/activity'>
       Click to go to Activity
       </Link>
