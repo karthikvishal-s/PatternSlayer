28d938847f70c3d1da7807f853d41992603370be
Karthik Vishal <karthikvishal1506@gmail.com>
2025-07-20 23:02:36 +0530

    Edited schooling details

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
         <div>
-            <h1>Hello</h1>
+            <h1>Welcome</h1>
+            <p>This is the home page of our React application.</p>
+            <p>Click the link below to navigate to the Activity page.</p>
+
         </div>
 
 
